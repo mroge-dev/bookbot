a76dd3b308a856ee0eec1abb946d641c16e8ce6e
mroge-dev <roge1.dev@gmail.com>
2025-05-08 09:44:42 -0500

    refactor get_number_words function stats.py file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_number_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -9,10 +11,5 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-def get_number_words(text):
-    words = text.split()
-    return len(words)
-    
- 
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_number_words(text):
+    words = text.split()
+    return len(words)
